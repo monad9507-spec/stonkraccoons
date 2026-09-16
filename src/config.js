6f69c4c117953db7ed00a3af18a8d8c1daa142b7
monad9507-spec <monad9507@gmail.com>
2026-09-16 15:30:34 +0300

    Update config.js

--- src/config.js
+++ src/config.js
@@ -11,10 +11,10 @@ export const ARC = {
 export const PROJECT_ID = "4f71172824a0ea69b0270161482356fe";
 
 // Paste only the deployed StonkRaccoons NFT contract address here.
-export const NFT_CONTRACT = "";
+export const NFT_CONTRACT = "0x6E49e473FebD8937F2387b68fC6fa39B6266f2De";
 
 // Replace these two links with your official X profile and OpenSea collection.
-export const X_URL = "https://x.com/";
+export const X_URL = "https://x.com/STONKRACCOONS";
 export const OPENSEA_URL = "https://opensea.io/";
 
 export const NFT_ABI = [
